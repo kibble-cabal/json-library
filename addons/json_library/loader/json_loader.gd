954787be179060162ffd362dc7405efabc3d5c0a
Audrey Serene <audrey.serene@gmail.com>
2023-06-26 22:04:27 -0500

    Write documentation for JSON loader classes (resolves #12)

--- addons/json_library/loader/json_loader.gd
+++ addons/json_library/loader/json_loader.gd
@@ -1,36 +1,50 @@
 class_name JsonLoader extends Object
 
-var _path: String
-var _output = {}
-var _default_dict: Dictionary = {}
-var _validator: JsonValidator = null
+## Handles loading (and optionally validating) JSON files
 
-var _config := {
-	keys_to_snake_case = false
-}
+## The path to load the JSON from
+var path: String
 
-func _init(path: String) -> void:
-	_path = path
+## The [Dictionary] or [Object] to mutate with the loaded data
+var output = {}
+
+## The [Dictionary] that contains default data, in case of an issue during [method load]
+var default_dict: Dictionary = {}
+
+## The [JsonValidator] to use to validate data after [method load]. If [code]null[/code], validation
+## will be skipped
+var validator: JsonValidator = null
+
+## If [code]true[/code], JSON keys will be converted to snake case during [method load]. This means
+## that you don't need to worry about differing naming conventions in JSON vs GDscript files.
+var config_keys_to_snake_case = false
 
 
-func output(object) -> JsonLoader:
-	_output = object
+func _init(path_value: String) -> void:
+	path = path_value
+
+
+## Set the [member output] to the provided [code]object[/code]. Can be a [Dictionary] or [Object].
+func set_output(object) -> JsonLoader:
+	output = object
 	return self
 
 
-func default_dict(default_value) -> JsonLoader:
-	_default_dict = default_value
+## Set the [member default_dict] to be used during [method load]
+func set_default_dict(default_value) -> JsonLoader:
+	default_dict = default_value
 	return self
 
 
-func validator(validator_value: JsonValidator = null) -> JsonLoader:
-	_validator = validator_value
+## Set the [member validator] to be used during [method load]
+func set_validator(validator_value: JsonValidator = null) -> JsonLoader:
+	validator = validator_value
 	return self
 
 
-## When loading JSON, all KEYS will be recursively converted to snake case (values will be unaffected)
+## When loading JSON, all KEYS will be recursively converted to snake case (values will be unaffected) (see [member config_keys_to_snake_case])
 func to_snake_case(keys_to_snake_case_value := true) -> JsonLoader:
-	_config.keys_to_snake_case = keys_to_snake_case_value
+	config_keys_to_snake_case = keys_to_snake_case_value
 	return self
 
 
@@ -42,32 +56,36 @@ func _convert_json_to_snake_case(dict: Dictionary) -> Dictionary:
 	)
 
 
+## 1. Loads the file at [member path].
+## 2. If [member config_keys_to_snake_case] is [code]true[/code], converts data keys to snake case
+## 3. If a [member validator] is provided, the loaded data will be validated and cleaned (see [method JsonValidator.is_valid] and [method JsonValidator.cleaned_data]).
+## 4. [member output] will be mutated with the loaded data and returned. This means that you can load JSON that effects node properties and other cool things.
 func load():
-	if FileAccess.file_exists(_path):
+	if FileAccess.file_exists(path):
 		# Read file
-		var file := FileAccess.open(_path, FileAccess.READ)
+		var file := FileAccess.open(path, FileAccess.READ)
 		var string := file.get_as_text()
 		var data := JSON.parse_string(string)
 		
-		var data_dict: Dictionary = data if data != null else _default_dict
+		var data_dict: Dictionary = data if data != null else default_dict
 		
 		# Convert to snake case, if necessary
-		if _config.keys_to_snake_case:
+		if config_keys_to_snake_case:
 			data_dict = _convert_json_to_snake_case(data_dict)
 		
 		# Validate data
 		var cleaned_data
-		if _validator:
-			if data_dict and _validator.is_valid(data): 
-				cleaned_data = _validator.cleaned_data(data, _default_dict)
+		if validator:
+			if data_dict and validator.is_valid(data): 
+				cleaned_data = validator.cleaned_data(data, default_dict)
 		else: cleaned_data = data_dict
 		
 		# Mutate output
 		if cleaned_data is Dictionary:
-			if _output is Object:
+			if output is Object:
 				for key in cleaned_data:
-					if key in _output: _output[key] = cleaned_data[key]
-			elif _output is Dictionary: _output.merge(cleaned_data, true)
-		else: _output = cleaned_data
+					if key in output: output[key] = cleaned_data[key]
+			elif output is Dictionary: output.merge(cleaned_data, true)
+		else: output = cleaned_data
 	
-	return _output
+	return output
--- addons/json_library/loader/json_schema_loader.gd
+++ addons/json_library/loader/json_schema_loader.gd
@@ -1,6 +1,8 @@
 class_name JsonSchemaLoader extends JsonLoader
 
+## Handles loading and creating validators from JSON schemas
 
+## Loads the JSON (see [method JsonLoader.load]) and (if successful) creates a [JsonValidator] from the loaded [Dictionary]
 func load_validator() -> JPropertyValidator:
 	var schema = super.load()
 	if schema is Dictionary: return JPropertyValidator.from_schema(schema)
